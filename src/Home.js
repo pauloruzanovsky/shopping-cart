04c84b167d1d9590cb297f8120dcef1c849e132d
Paulo Ruzanovsky <pauloruzanovsky@gmail.com>
2023-04-07 17:31:15 -0300

    adjust css

--- src/Home.js
+++ src/Home.js
@@ -6,12 +6,14 @@ import './style/Home.css'
 export default function Home() {
     return(
         <>
-            <div className="wrapper">
+            <div className="home-wrapper">
+                <div className='home-text'>
+                    <div className='home-menu'>Healthy, flavorous and fast.</div>
+                    <Link to='/shopping-cart/menu'>
+                        <div className='home-menu2'>Click here and check our menu!</div>
+                    </Link>
+                </div>
                 <img className='home-img' src={homeImg}/>
-                <div className='home-menu'>Healthy, flavorous and fast.</div>
-                <Link to='/shopping-cart/menu'>
-                    <div className='home-menu2'>Check our menu</div>
-                </Link>
             </div>
         </>
     )
